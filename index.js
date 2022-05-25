4366a85c223dbae4b6e0d1c13efbb8b0b1fe14b2
Mark Delgado <markdelgado.md@hotmail.com>
2022-05-25 00:05:32 -0400

    commit tree 17360f65402c7b45d5f5360729e74b458cc2cb64

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+ 
+function destructivelyAppendCat(name) {
+    cats.push("Ralph");
+    return cats; }
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+}
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+function appendCat(name){
+    var name_1= [...cats,name];
+    return name_1;}
+function prependCat(name){
+    var name_2 = [name, ...cats]
+    return name_2
+}
+function removeLastCat() {
+    var newcats= cats.slice(0, cats.length-1);
+    return newcats
+}
+function removeFirstCat() {
+    var newCats= cats.slice(1)
+    return newCats
+}
